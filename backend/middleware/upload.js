07df2145f95e0153b359b747e2dc66b36997b846
fobetow823-sketch <fobetow823@gmail.com>
2025-09-25 11:39:19 +0530

    middleware fix

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,21 +1,4 @@
-// const multer = require("multer");
-// const { CloudinaryStorage } = require("multer-storage-cloudinary");
-// const cloudinary = require(".//config/cloudinaryConfig");
 
-// const storage = new CloudinaryStorage({
-//   cloudinary: cloudinary,
-//   params: {
-//     folder: "profile_images",
-//     allowed_formats: ["jpg", "jpeg", "png"],
-//     transformation: [{ width: 500, height: 500, crop: "limit" }],
-//   },
-// });
-
-// const upload = multer({ storage });
-
-// module.exports = upload;
-
-// Upload.js
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 
